fc702ddb9f69d34d444a5592ce5c1f748070d0e4
Robert <127435909+robifal@users.noreply.github.com>
2023-08-08 15:06:50 -0300

    Adicionado exports e opção para retirar um id gato

--- src/cats/cats.controller.js
+++ src/cats/cats.controller.js
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Bind, Param, Res, HttpStatus, Delete } from '@nestjs/common';
+
+const GATOS = [
+    {
+        id: 1,
+        nome: "Luke",
+        corDoOlho: "Castanho claro",
+        raca: "Preto"
+    },
+    {
+        id: 2,
+        nome: "Estopa",
+        corDoOlho: "Azul",
+        raca: "Cianes"
+    },
+    {
+        id: 3,
+        nome: "Nilo",
+        corDoOlho: "Azul",
+        raca: "Branco"
+    }
+]
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+    @Get()
+    findAll(){
+        return GATOS
+    }
+
+    @Get(':id')
+    @Bind(Param(), Res())
+    findOne(params, res){
+        const gatoEncontrado = GATOS.find(gato => gato.id == params.id)
+        if(gatoEncontrado){
+            res.status(HttpStatus.OK).json(gatoEncontrado)
+            // return gatoEncontrado
+        }
+        else{
+            res.status(HttpStatus.NOT_FOUND).send()
+            // return "gato não encontrado"
+        }
+       
+    }
+
+    @Delete(':id')
+    @Bind(Param('id'), Res())
+    remove(id, res){
+        const indexGatoEncontrado = GATOS.findIndex(gato => gato.id == id)
+        if(indexGatoEncontrado >= 0){
+            GATOS.splice(indexGatoEncontrado, 1)
+            res.status(HttpStatus.NO_CONTENT).send()
+        }
+        else{
+            res.status(HttpStatus.NOT_FOUND).send()
+        }
+    }
+}

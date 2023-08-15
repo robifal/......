8bf0331f1c52c5a268d2bd147139bf23899bd3bf
Robert <127435909+robifal@users.noreply.github.com>
2023-08-15 16:29:48 -0300

    Foi adicionado o delete com index.

--- src/cats/cats.controller.js
+++ src/cats/cats.controller.js
@@ -53,9 +53,9 @@ export class CatsController {
     @Delete(':id')
     @Bind(Param('id'), Res())
     remove(id, res){
-        const indexGatoEncontrado = GATOS.findIndex(gato => gato.id == id)
+        const indexGatoEncontrado = this.catsService.findIndex(id)
         if(indexGatoEncontrado >= 0){
-            GATOS.splice(indexGatoEncontrado, 1)
+           this.catsService.deleteByIndex(indexGatoEncontrado)
             res.status(HttpStatus.NO_CONTENT).send()
         }
         else{
